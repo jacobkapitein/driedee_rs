258ec57ff0b0094a6cd34bdd331af996b2882f3f
Jacob Kapitein <jacob.kapitein@protonmail.com>
2025-01-16 22:48:25 +0100

    add max fps option

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use core::engine::Engine;
+use clap::{Arg, ArgMatches, Command};
 use sdl2::event::{Event, WindowEvent};
 use sdl2::keyboard::Keycode;
 use std::time::{Instant, Duration};
@@ -7,6 +8,8 @@ use std::thread::sleep;
 mod core;
 
 fn main() -> Result<(), String> {
+    let matches = set_commands();
+
     let mut engine: Engine = Engine::new("3D Engine", 1280, 720);
 
     // Create the SDL event pump to handle events
@@ -16,8 +19,11 @@ fn main() -> Result<(), String> {
         .expect("Error creating event pump");
 
     // Set the target frame rate (FPS)
-    let target_fps = 60;
-    let target_frame_duration = Duration::from_secs_f32(1.0 / target_fps as f32);
+    let target_fps = matches
+        .get_one::<f32>("max-fps")
+        .cloned()
+        .unwrap_or(60.0);
+    let target_frame_duration = Duration::from_secs_f32(1.0 / target_fps);
 
     // Main loop
     let mut running = true;
@@ -65,4 +71,17 @@ fn main() -> Result<(), String> {
     }
 
     Ok(())
+}
+
+fn set_commands() -> ArgMatches {
+    Command::new("driedee_rs")
+        .about("A basic 3D engine application")
+        .arg(
+            Arg::new("max-fps")
+                .long("max-fps")
+                .value_parser(clap::value_parser!(f32))
+                .default_value("60") // Default FPS if not provided
+                .help("Sets the maximum frames per second"),
+        )
+        .get_matches()
 }
